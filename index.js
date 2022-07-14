ddf9e433721e0561a4d1ce0754050332bcdc0919
Mohamd Imad <medoimad@hotmail.com>
2022-07-13 23:20:51 -0400

    Add functions to each of the buttons Change the names of the buttons to Q W E R T Y U Make sure to add the "onClick" options on the html section for ewach button and call the function Therefore, when you click a button the function will execute

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-function codeAddress() {
-            alert('Hello');
-        }
-window.onload = codeAddress;
+// function codeAddress() {
+//             alert('Hello');
+//         }
+// window.onload = codeAddress;
+
+
+function Q() {
+  alert('W has been clicked')
+}
+
+function W() {
+  alert('W has been clicked')
+}
+
+function E() {
+  alert('E has been clicked')
+}
+
+function R() {
+  alert('R has been clicked')
+}
+
+function T() {
+  alert('T has been clicked')
+}
+
+function Y() {
+  alert('Y has been clicked')
+}
+
+function U() {
+  alert('U has been clicked')
+}
