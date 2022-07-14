22d1eaae97d8332ede4b2ccd30a3187eb02f7c72
Mohamd Imad <medoimad@hotmail.com>
2022-07-14 08:38:31 -0400

    Add mp3 files for each button Activate each button when its clicked. By activate I mean the assigned sound must play

--- index.js
+++ index.js
@@ -1,33 +1,56 @@
+// TESTING A RANDOM FUNCTION IF THE JS LOADS OR NOT
 // function codeAddress() {
 //             alert('Hello');
 //         }
 // window.onload = codeAddress;
 
 
+
+// PLAYING RANDOM SOUNDS
+// To play sound you can either write the following inside a function:
+// -Option 1:  var audioo= new Audio('ominous-drums-appear.mp3').play();
+//    audioo.play();
+// -Option 2. new Audio('ominous-drums-appear.mp3').play();
+// -Note: the new Audio constrcutors (a function that initizlises an instance) is important
+
+
+// Option 1
 function Q() {
-  alert('W has been clicked')
-}
+  var q = new Audio('Q_KnockingSubBass.mp3');
+  q.play();}
 
+// Option 2
 function W() {
-  alert('W has been clicked')
+    new Audio('W_LosingDrums.mp3').play();
 }
 
+// Option 1
 function E() {
-  alert('E has been clicked')
+  var e = new Audio('E_TribalDrumEchos.mp3');
+  e.play();
 }
 
+// Option 1
 function R() {
-  alert('R has been clicked')
+  var r = new Audio('R_FailDrumAndBells.mp3');
+  r.play();
+
 }
 
+// Option 1
 function T() {
-  alert('T has been clicked')
+  var t = new Audio('TJokesDrums.mp3');
+  t.play();
 }
 
+// Option 1
 function Y() {
-  alert('Y has been clicked')
+  var y = new Audio('YDeepDarkHorrorDrum.mp3');
+  y.play();
 }
 
+// Option 1
 function U() {
-  alert('U has been clicked')
+  var u = new Audio('UFunnyCartoonTones.mp3');
+  u.play();
 }
