a8a55d2eb5fc99e9762ff749073e51a83313cee6
Mohamd Imad <medoimad@hotmail.com>
2022-07-25 23:20:38 -0400

    Add the keyboard functionality of the site

--- index.js
+++ index.js
@@ -16,10 +16,15 @@
 // -Note: the new Audio constrcutors (a function that initizlises an instance) is important
 
 
+
 // Option 1
 function Q() {
   var q = new Audio('QJokesDrums.mp3');
-  q.play();}
+  q.play();
+
+
+
+}
 
 // Option 2
 function W() {
@@ -56,3 +61,44 @@ function U() {
   var u = new Audio('Uharmonica.mp3');
   u.play();
 }
+
+
+// We may use x.key = "q" or x.keyCode="81".  The 81 covers the capital/lower letters
+document.addEventListener("keydown", function(x)  {
+  if (x.keyCode == "81" || x.keyCode == "81")
+  new Audio('QJokesDrums.mp3').play();
+  else if (x.key == "w" || x.key == "W")
+  new Audio('Wpiano.mp3').play();
+  else if (x.key == "e" || x.key == "E")
+  new Audio('Eflute.mp3').play();
+  else if (x.key == "r" || x.key == "R")
+  new Audio('Rguitar.mp3').play();
+  else if (x.key == "t" || x.key == "T")
+  new Audio('Tviolin.mp3').play();
+  else if (x.key == "y" || x.key == "Y")
+  new Audio('Yharp.mp3').play();
+  else if (x.key == "u" || x.key == "U")
+  new Audio('Uharmonica.mp3').play();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// Notes:
+// // This is a simple code that can tell us which key is pressed when we press anything on the keyboard
+// // The while document is listening. Which is why we use document.addEventListener
+// document.addEventListener("keydown", function(x)  {
+//   console.log(x)
+// });
